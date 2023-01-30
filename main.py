480b6345e63f9176e198fdb7dac1644dc8a88395
yuri timao <yuri-timao2011@hotmail.com>
2023-01-29 21:37:03 -0300

    Adjust in functions of download

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import requests
 import os
 import random
 from Ui.DownUi import Ui_MainWindow
-from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QComboBox
+from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog
 from PyQt6.QtGui import QPixmap
 from pytube import YouTube
 from Avisos import Avisos
 
 
 def download_thumb(title, link):
+    symbols = ['[',']','(',')','"','!','|']
+    for i in symbols:
+        if i in title:
+            title = title.replace(i,'')
     f = open('img/' + title + '.jpg', 'wb')
     response = requests.get(link)
     f.write(response.content)
     f.close()
+    return title
 
 
 class Downtube(QMainWindow, Ui_MainWindow):
@@ -21,11 +26,12 @@ class Downtube(QMainWindow, Ui_MainWindow):
         super().__init__(parent)
         super().setupUi(self)
         self.setavalue()
-        self.pushButton.clicked.connect(self.search_video)
+        self.btn_search.clicked.connect(self.search_video)
+        self.btn_download.clicked.connect(self.down_video)
 
     def setavalue(self) -> None:
-        self.comboBox.addItems(['360p', '480p', '720p', '1080p'])
-        self.progressBar.setValue(0)
+        self.cb_res.addItems(['360p', '480p', '720p', '1080p'])
+
 
     def search_video(self) -> None:
         if len(self.lineEdit.text()) == 0:
@@ -34,8 +40,39 @@ class Downtube(QMainWindow, Ui_MainWindow):
         else:
             yt = YouTube(self.lineEdit.text())
             self.title_video.setText(yt.title)
-            download_thumb(yt.title, yt.thumbnail_url)
-            self.label.setPixmap(QPixmap('img/' + yt.title + '.jpg'))
+            title = download_thumb(yt.title, yt.thumbnail_url)
+
+            self.label.setPixmap(QPixmap('img/' + title + '.jpg'))
+
+    def down_video(self) -> None:
+        match self.cb_res.currentText():
+            case '360p':
+                self.lbl_video_status.setText('Baixando aguarde...')
+                file = QFileDialog.getExistingDirectory(caption='Selecione a pasta')
+                youtube = YouTube(self.lineEdit.text())
+                youtube.streams.filter(file_extension='mp4').get_by_resolution(self.cb_res.currentText()).download(output_path=file)
+                self.lbl_video_status('Sucesso!')
+            case '480p':
+                self.lbl_video_status.setText('Baixando aguarde...')
+                file = QFileDialog.getExistingDirectory(caption='Selecione a pasta')
+                youtube = YouTube(self.lineEdit.text())
+                youtube.streams.filter(file_extension='mp4').get_by_resolution(self.cb_res.currentText()).download(
+                    output_path=file)
+                self.lbl_video_status('Sucesso!')
+            case '720p':
+                self.lbl_video_status.setText('Baixando aguarde...')
+                file = QFileDialog.getExistingDirectory(caption='Selecione a pasta')
+                youtube = YouTube(self.lineEdit.text())
+                youtube.streams.filter(file_extension='mp4').get_by_resolution(self.cb_res.currentText()).download(
+                    output_path=file)
+                self.lbl_video_status('Sucesso!')
+            case '1080p':
+                self.lbl_video_status.setText('Baixando aguarde...')
+                file = QFileDialog.getExistingDirectory(caption='Selecione a pasta')
+                youtube = YouTube(self.lineEdit.text())
+                youtube.streams.filter(file_extension='mp4').get_by_resolution(self.cb_res.currentText()).download(
+                    output_path=file)
+                self.lbl_video_status('Sucesso!')
 
 
 if __name__ == '__main__':
